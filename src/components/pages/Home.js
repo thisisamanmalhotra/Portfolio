c30793bba5b3dcf0b2ce5db7b5d4ee3ea4626552
Vivek Mittal <42748450+vivek1909@users.noreply.github.com>
2020-03-10 00:50:14 +0530

    #resume-added

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 
 import Card from "./Card";
 import vivek from "./../../assets/images/vivek-min.png";
+import resume from "./../../assets/Resume.pdf";
 
 const Main = styled.div`
   display: flex;
@@ -87,6 +88,74 @@ const Main = styled.div`
       content: "Self-taught";
     }
   }
+
+  .col3 {
+    padding: 3rem;
+    display: flex;
+    flex-basis: 100%;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .btn {
+    background:transparent;
+    text-align:center;
+    letter-spacing:1px;
+    font-size:1.4em;
+    line-height:1.2em;
+    border:5px solid #089;
+    overflow:hidden;
+    transition:all 0.5s;
+    border-radius: 4px;
+  }
+
+  .btn:hover, .btn:active {
+    text-decoration: none;
+    color:#fff;
+    border-color:#089;
+    background:#089;
+  }
+
+  .btn-label {
+    display:inline-block;
+    position:relative;
+    padding-right:0;
+    transition:padding-right 0.5s;
+    color:#089;
+    font-family: "Montserrat Alternates";
+    font-weight: bold;
+  }
+
+  .btn-label:after {
+    content: "";
+    position:absolute;
+    right:-50px;
+    opacity:0;
+    top:22%;
+    width:10px;
+    height:10px;
+    margin-right:-10px;
+    background:rgba(0,0,0,0);
+    border:3px solid #fff;
+    border-top:none;
+    border-right:none;
+    transition: opacity 0.5s, top 0.5s, right 0.5s;
+    transform:rotate(225deg);
+  }
+
+  .btn:hover .btn-label,
+  .btn:active .btn-label {
+    padding-right:30px;
+    color:#f5f1e7;
+  }
+
+  .btn:hover .btn-label:after,
+  .btn:active .btn-label:after {
+    transition:opacity: 0.5s, top 0.5s, right 0.5s;
+    opacity:1;
+    right:10px;
+    top:22%;
+  }
 `;
 
 const Home = () => {
@@ -111,6 +180,13 @@ const Home = () => {
         </div>
         <div className="col2">
           <img src={vivek} alt="Vivek" width="308px" height="308px" />
+        </div>
+        <div className="col3">
+          <button className="btn">
+            <a href={resume}>
+              <span class="btn-label">RESUME</span>
+            </a>
+          </button>
         </div>
       </div>
       <Card />
